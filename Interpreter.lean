c4ae552480d950b79972d6ec8540e0982284f665
Yonatan Reicher <yony252525@gmail.com>
2025-03-08 12:50:43 +0200

    Add Interpreter.Lex

--- Interpreter.lean
+++ Interpreter.lean
@@ -1,4 +1,5 @@
 -- This module serves as the root of the `InterpreterTest` library.
 -- Import modules here that should be built as part of the library.
 import Interpreter.Basic
+import Interpreter.Lex
 import Interpreter.Parse
