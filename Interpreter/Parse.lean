b35f9ffad6c4b5fcb7e9a64fd323f126550409c4
Yonatan Reicher <yony252525@gmail.com>
2025-03-08 12:51:18 +0200

    Update

--- Interpreter/Parse.lean
+++ Interpreter/Parse.lean
@@ -1,4 +1,20 @@
 import Interpreter.Ast
+import Interpreter.Token
 
-import Interpreter.Parse.Basic
-import Interpreter.Parse.Lemmas
+abbrev Tokens := List Token
+
+def expr : StateM Tokens (Option Ast) := do
+  match <- get with
+  | Token.int val :: rest => do
+    set rest
+    pure (some (Ast.val val))
+  | _ => pure none
+
+def parse : List Token -> Ast := fun tokens =>
+  match expr.run tokens with
+  | (some ast, []) => ast
+  | (some _, _) => panic! "Leftover tokens: {rest}"
+  | (none, _) => panic! "Not a beginning of an expression"
+  
+example
+: parse [Token.int 10] = Ast.val 10 := rfl
