99ea0fe514c46e385af3e2408ed9c1a9f05c3e04
Yonatan Reicher <yony252525@gmail.com>
2025-04-30 18:23:34 +0300

    Add examples and change let_ to letIn in Ast

--- Interpreter/Types/Ast.lean
+++ Interpreter/Types/Ast.lean
@@ -2,8 +2,12 @@ import Interpreter.Types.Identifier
 
 
 inductive Ast : Type where
+  --- 123
   | val : Int → Ast
+  --- x
   | var : Ident → Ast
+  --- x + y
   | add : Ast → Ast → Ast
-  | let_ : Ident → Ast → Ast → Ast
+  --- let x := 123 in x + y
+  | letIn : Ident → Ast → Ast → Ast
   deriving Repr, BEq, Inhabited
