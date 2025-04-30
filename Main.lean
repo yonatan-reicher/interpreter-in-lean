77fa9415d314d8ca2be87f88d09fb2eb7c619291
Yonatan Reicher <yony252525@gmail.com>
2025-04-30 18:25:05 +0300

    Refactor main

--- Main.lean
+++ Main.lean
@@ -2,6 +2,17 @@ import Interpreter
 
 
 open Std
+open Interpreter
+
+
+inductive MError
+  | lex : Lex.Error -> MError
+  | parse : Parse.Error -> MError
+  | check : Error -> MError
+  deriving Inhabited, Repr
+instance : Coe Lex.Error MError := ⟨.lex⟩
+instance : Coe Parse.Error MError := ⟨.parse⟩
+instance : Coe Error MError := ⟨.check⟩
 
 
 def IO.FS.Stream.getLines (stream : IO.FS.Stream) : IO (Array String) := do
@@ -20,13 +31,32 @@ def IO.FS.Stream.readText (stream : IO.FS.Stream) : IO String := do
   |>.map fun lines => lines.foldl (fun acc line => acc ++ line) "\n"
 
 
-def parseStream (stream : IO.FS.Stream) : ExceptT Unit IO $ (type : Ty) × (Expr type ∅) := do
+def coeError {ε α ε'} [∀ e, CoeT ε e ε'] (x : Except ε α) : Except ε' α :=
+  x.mapError fun (e : ε) => (↑ e : ε')
+
+
+-- TODO: Rename
+def arstarts (code : String)
+: Except MError $ (type : Ty) × (Expr type ∅) := do
+  let tokens <- Lex.lex code.toList |> coeError
+  dbg_trace s!"Tokens: {repr tokens}"
+  let ast <- parse tokens |> coeError
+  dbg_trace s!"Ast: {repr ast}"
+  let expr <- check ast |> coeError
+  dbg_trace s!"Expr: {repr expr}"
+  return expr
+
+
+-- def parseStream (stream : IO.FS.Stream) : ExceptT Unit IO $ (type : Ty) × (Expr type ∅) := do
+def parseStream (stream : IO.FS.Stream) : ExceptT MError IO $ Ast := do
   let code <- stream.readText
-  let tokens <- Lex.lex code.toList |>.mapError fun _ => ()
-  let ast := parse tokens
-  sorry
+  let tokens <- Lex.lex code.toList |>.mapError fun (e : Lex.Error) => (e : MError)
+  let ast <- parse tokens |>.mapError fun (e : Parse.Error) => (e : MError)
+  return ast
+
 
 def main : IO Unit := do
   let stream ← IO.getStdin
-  let expr ← parseStream stream
-  IO.println s!"Parsed: {repr expr}"
+  let code <- stream.readText
+  let expr := arstarts code
+  IO.println s!"Expr: {repr expr}"
