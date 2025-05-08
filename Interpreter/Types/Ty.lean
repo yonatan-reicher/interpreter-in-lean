cdabb59407da6ccf61a21856b02ca0946ee7d1bc
Yonatan Reicher <yony252525@gmail.com>
2025-05-08 20:38:15 +0300

    Ty instances

--- Interpreter/Types/Ty.lean
+++ Interpreter/Types/Ty.lean
@@ -5,3 +5,10 @@ inductive Ty
   | int
   | bool
   deriving Inhabited, DecidableEq, Hashable, Repr
+
+
+def Ty.toFormat : Ty -> Std.Format
+    | .int => "int"
+    | .bool => "bool"
+instance : Std.ToFormat Ty := ⟨Ty.toFormat⟩
+instance : ToString Ty := ⟨fun ty => ty.toFormat.pretty⟩
