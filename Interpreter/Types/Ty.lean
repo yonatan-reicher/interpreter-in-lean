9a0dfa720fc84423c52b2fcdf51cd7a234a34ebd
Yonatan Reicher <yony252525@gmail.com>
2025-05-08 20:38:24 +0300

    Add Ty.func

--- Interpreter/Types/Ty.lean
+++ Interpreter/Types/Ty.lean
@@ -4,11 +4,20 @@ namespace Interpreter.Types
 inductive Ty
   | int
   | bool
+  | func (arg ret : Ty)
   deriving Inhabited, DecidableEq, Hashable, Repr
 
 
 def Ty.toFormat : Ty -> Std.Format
     | .int => "int"
     | .bool => "bool"
+    | .func arg ret =>
+      if needsParens arg then
+        f!"({arg.toFormat}) -> {ret.toFormat}"
+      else
+        f!"{arg.toFormat} -> {ret.toFormat}"
+    where needsParens
+      | .func _ _ => true
+      | _ => false
 instance : Std.ToFormat Ty := ⟨Ty.toFormat⟩
 instance : ToString Ty := ⟨fun ty => ty.toFormat.pretty⟩
